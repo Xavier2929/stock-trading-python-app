955b61e125f10403971777b3edab0ac77fff80bf
xavier2929 <castilloxavier49@gmail.com>
2025-09-12 18:18:34 -0700

    UPDATE: BUG FIX, error when trying to get data results when there was no data results.

--- script.py
+++ script.py
@@ -1,52 +1,52 @@
 import requests
-import os
 import csv
-
+import os
 from dotenv import load_dotenv
-
 load_dotenv()
 
 POLYGON_API_KEY = os.getenv("POLYGON_API_KEY")
+
 LIMIT = 1000
 
 url = f'https://api.polygon.io/v3/reference/tickers?market=stocks&active=true&order=asc&limit={LIMIT}&sort=ticker&apiKey={POLYGON_API_KEY}'
 response = requests.get(url)
 tickers = []
-data = response.json()
-next_url = data["next_url"]
 
+data = response.json()
 for ticker in data['results']:
     tickers.append(ticker)
-    
-while "next_url" in data:
-    print('requesting next page',data['next_url'])
-    response = requests.get(data['next_url']+f'&apiKey={POLYGON_API_KEY}')
+
+while 'next_url' in data:
+    print('requesting next page', data['next_url'])
+    response = requests.get(data['next_url'] + f'&apiKey={POLYGON_API_KEY}')
+    data = response.json()
+
+    if 'results' not in data:
+        print("No 'results' in response:", data)
+        break
+
     for ticker in data['results']:
         tickers.append(ticker)
 
-example_ticker = {
-    'ticker':'ZWS',
-    'name': 'Zurn Elkay Water Solutions Corporation',
-    'market': 'stocks',
-    'locale':'us',
-    'primary_exchange':'XNYS',
-    'type':'CS',
-    'active': True,
-    'currency_name':'usd',
-    'cik':'0001439288',
-    'composite_figi':'BBG000H8R0N8',
-    'share_class_figi':"BBG00"
-}
-
-#WRITE THE TICKERS INTO A CSV FILE UNDER THE EXAMPLE SCHEMA MADE BEFORE
+example_ticker = {'ticker': 'ZWS',
+                  'name': 'Zurn Elkay Water Solutions Corporation',
+                  'market': 'stocks',
+                  'locale': 'us',
+                  'primary_exchange': 'XNYS',
+                  'type': 'CS',
+                  'active': True,
+                  'currency_name': 'usd',
+                  'cik': '0001439288',
+                  'composite_figi': 'BBG000H8R0N8',
+                  'share_class_figi': 'BBG001T36GB5',
+                  'last_updated_utc': '2025-09-11T06:11:10.586204443Z'}
 
 fieldnames = list(example_ticker.keys())
 output_csv = 'tickers.csv'
-
-with open(output_csv,mode='w',newline='',encoding='utf-8') as file:
-    writer = csv.DictWriter(file,fieldnames=fieldnames)
+with open(output_csv, mode='w', newline='', encoding='utf-8') as file:
+    writer = csv.DictWriter(file, fieldnames=fieldnames)
     writer.writeheader()
-    for ticker in tickers:
-        row = {key: ticker.get(key,'') for key in fieldnames}
+    for t in tickers:
+        row = {key: t.get(key, '') for key in fieldnames}
         writer.writerow(row)
 print(f'Wrote {len(tickers)} rows to {output_csv}')
